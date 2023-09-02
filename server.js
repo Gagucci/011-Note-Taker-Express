f07574e465f1e2e5af00072947f7f0796f16a496
Gagucci <126989750+Gagucci@users.noreply.github.com>
2023-09-02 11:11:22 -0700

    added nodemon + started api routes

--- server.js
+++ server.js
@@ -3,7 +3,16 @@ const fs = require("fs");
 const path = require("path");
 const express = require("express");
 
+// variables to require the html routes
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// middleware to parse incoming data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
